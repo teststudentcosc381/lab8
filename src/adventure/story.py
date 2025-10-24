c3221074e2f830150c263cbc20fdaa394008d367
Test Student <144061513+teststudentcosc381@users.noreply.github.com>
2025-10-24 17:24:32 +0000

    format

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,7 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.console import Console
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,20 +11,21 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[black]You stand still, unsure what to do. The forest swallows you.[/black]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[red]You walk left. " + event + "[/red]"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[blue]You walk right. " + event + "[/blue]"
 
 if __name__ == "__main__":
+    console = Console()
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[green]You wake up in a dark forest. You can go left or right.[/green]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = console.input("[italic yellow]Which direction do you choose?[/italic yellow] (left/right/exit): ")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
